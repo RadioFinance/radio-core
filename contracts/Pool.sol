6dc0723ba15c6dba09301ec783b5cf36f817e998
RadioFinance <RadioFinance@hotmail.com>
2021-03-17 23:04:56 +0800

    init

---
+++ contracts/Pool.sol
@@ -0,0 +1,192 @@
+pragma solidity ^0.6.0;
+/**
+ *Submitted for verification at Etherscan.io on 2020-07-17
+ */
+
+/*
+   ____            __   __        __   _
+  / __/__ __ ___  / /_ / /  ___  / /_ (_)__ __
+ _\ \ / // // _ \/ __// _ \/ -_)/ __// / \ \ /
+/___/ \_, //_//_/\__//_//_/\__/ \__//_/ /_\_\
+     /___/
+
+* Synthetix: BASISCASHRewards.sol
+*
+* Docs: https://docs.synthetix.io/
+*
+*
+* MIT License
+* ===========
+*
+* Copyright (c) 2020 Synthetix
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+*/
+
+import '@openzeppelin/contracts/math/Math.sol';
+import '@openzeppelin/contracts/math/SafeMath.sol';
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+import '@openzeppelin/contracts/utils/Address.sol';
+import '@openzeppelin/contracts/token/ERC20/SafeERC20.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+import './LPTokenWrapper.sol';
+
+contract Pool is LPTokenWrapper, Ownable
+{
+    IERC20 public radio;
+    uint256 constant public OneDay = 1 days;
+
+    uint256 public starttime;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+    address public minerOwner;
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+
+    constructor(
+        address radio_,
+        address lptoken_,
+        uint256 starttime_,
+        address minerOwner_
+    ) public {
+        radio = IERC20(radio_);
+        lpt = IERC20(lptoken_);
+        starttime = starttime_;
+        minerOwner = minerOwner_;
+    }
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+        _;
+    }
+
+    modifier checkStart() {
+        require(block.timestamp >= starttime, 'Pool: not start');
+        _;
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return Math.min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+        rewardPerTokenStored.add(
+            lastTimeRewardApplicable()
+            .sub(lastUpdateTime)
+            .mul(rewardRate)
+            .mul(1e18)
+            .div(totalSupply())
+        );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+        balanceOf(account)
+        .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+        .div(1e18)
+        .add(rewards[account]);
+    }
+
+    function stake(uint256 amount)
+    public
+    override
+    updateReward(msg.sender)
+    checkStart
+    {
+        require(amount > 0, 'Pool: Cannot stake 0');
+        super.stake(amount);
+        emit Staked(msg.sender, amount);
+    }
+
+    function withdraw(uint256 amount)
+    public
+    override
+    updateReward(msg.sender)
+    checkStart
+    {
+        require(amount > 0, 'Pool: Cannot withdraw 0');
+        super.withdraw(amount);
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function exit() external {
+        withdraw(balanceOf(msg.sender));
+        getReward();
+
+    }
+
+    function getReward() public updateReward(msg.sender) checkStart {
+        uint256 reward = earned(msg.sender);
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            radio.safeTransferFrom(minerOwner, msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function transferBack(address back, uint256 amount) external onlyOwner {
+        radio.safeTransfer(back, amount);
+    }
+
+    function initSet(uint256 rewardPerDay, uint256 _periodFinish)
+    external
+    onlyOwner
+    updateReward(address(0))
+    {
+        require(block.timestamp < starttime, "block.timestamp < starttime");
+        require(starttime < _periodFinish, "starttime < _periodFinish");
+
+        rewardRate = rewardPerDay.div(OneDay);
+        periodFinish = _periodFinish;
+        lastUpdateTime = starttime;
+    }
+
+    function updateRewardRate(uint256 rewardPerDay, uint256 _periodFinish)
+    external
+    onlyOwner
+    updateReward(address(0))
+    {
+        if (_periodFinish == 0) {
+            _periodFinish = block.timestamp;
+        }
+
+        require(starttime < block.timestamp, "starttime < block.timestamp");
+        require(block.timestamp <= _periodFinish, "block.timestamp <= _periodFinish");
+
+        rewardRate = rewardPerDay.div(OneDay);
+
+        periodFinish = _periodFinish;
+        lastUpdateTime = block.timestamp;
+    }
+}
